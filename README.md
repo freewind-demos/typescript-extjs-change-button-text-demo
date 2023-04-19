dbe2d60b9cfa4236575fda8baa5f27fadf0bdc12
Freewind <nowindlee@gmail.com>
2023-04-19 22:18:42 +0800

    typescript-extjs-button-click-demo

--- README.md
+++ README.md
@@ -1,13 +1,7 @@
-TypeScript ExtJS Custom Events Demo
+TypeScript ExtJS Button Click Demo
 ===================================
 
-在ExtJs中可以定义任意名字的event，它们可能跟浏览器内置的相同，也可以完全不同。
-
-因为对于非浏览器event，需要靠代码调用来触发，实际上和调用一个方法差不多。
-
-在代码里，可以通过 `listeners: {...}`来匹配，也可以通过`component.fireEvent('custom-event', component)`来触发。
-
-比如本例中，可以通过`hello.app.mainView.fireEvent('myEvent', hello.app.mainView)`触发。
+Extjs中可以通过listeners来监听一些事件，某些事件与标准事件同名，比如`click`
 
 ```
 npm install
--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,11 @@
 Ext.onReady(() => {
-    Ext.define('MyView', {
-        extend: 'Ext.panel.Panel',
+    new Ext.button.Button({
+        text: 'button',
         renderTo: 'main',
-        height: 100,
-        width: 200,
-        title: 'Hello world',
-        controller: new Ext.app.ViewController({
-            onMyEvent: () => {
-                alert('### controller.onMyEvent');
-            }
-        }),
         listeners: {
-            myEvent: 'onMyEvent'
-        },
-        items: [
-            new Ext.button.Button({
-                text: 'button',
-                listeners: {
-                    click: 'onMyEvent'
-                }
-            })
-        ]
-    })
-    new Ext.app.Application({
-        name: 'hello',
-        mainView: Ext.create('MyView'),
-        launch: () => {
-            console.log("### launch")
-            return true;
+            click: () => {
+                alert('button clicked!')
+            }
         }
-    });
+    })
 })
